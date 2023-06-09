f188be47e392ec7640db3d2039420ea9d8dd7425
Mavya <mavyatekula12@gmail.com>
2023-06-09 15:21:21 -0500

    files added

--- Assign7/src/Reverseword.java
+++ Assign7/src/Reverseword.java
@@ -1,16 +1,19 @@
 public class Reverseword {
     public static void main(String[] args) {
-        int j;
-    String str="Java J2EE Reverse Me";
-    String s[]=str.split("");
-    String s1="";
+        String str = "Java J2EE Reverse Me";
+        String[] letters = str.split(" ");
 
-    for (int i=0;i<=s.length-1;i++){
-        s1=s[i];
-        s1=s1+" ";
-        for(j=s1.length()-1;j>=0;j--){
-            System.out.println(s1.charAt(j));
+        for (int i = 0; i < letters.length; i++) {
+            String reverseword = reverseword(letters[i]);
+            System.out.print(reverseword + " ");
         }
     }
+
+    public static String reverseword(String word) {
+        StringBuilder reversed = new StringBuilder();
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed.append(word.charAt(i));
+        }
+        return reversed.toString();
     }
 }
